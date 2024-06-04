35424f55d644905ff95a0e461173e17585a5b74b
carvalhaus <ooliveira.joaop@gmail.com>
2024-06-04 19:14:20 -0300

    fat: create GetPatientByPhone use case

---
+++ api/src/application/useCases/patient/GetPatientById.ts
@@ -0,0 +1,19 @@
+import DatabaseService from "@/infra/DatabaseService";
+
+export default class GetPatientByIdUseCase {
+  constructor(readonly database: DatabaseService) {}
+
+  async execute(phone: string) {
+    const INCLUDE_APPOINTMENT = true;
+    const patient = await this.database.getPatientByPhone(
+      phone,
+      INCLUDE_APPOINTMENT
+    );
+
+    if (!patient) {
+      throw new Error("No patient found");
+    }
+
+    return patient;
+  }
+}
