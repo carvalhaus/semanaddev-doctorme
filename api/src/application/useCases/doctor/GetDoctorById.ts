f9767d6324d3c335dc86c955db8fb7ffee35d302
carvalhaus <ooliveira.joaop@gmail.com>
2024-06-04 19:14:04 -0300

    fat: create GetDoctorById use case

---
+++ api/src/application/useCases/doctor/GetDoctorById.ts
@@ -0,0 +1,16 @@
+import DatabaseService from "@/infra/DatabaseService";
+
+export default class GetDoctorByIdUseCase {
+  constructor(readonly database: DatabaseService) {}
+
+  async execute(id: number) {
+    const INCLUDE_AGENDA = true;
+    const doctor = await this.database.getDoctorById(id, INCLUDE_AGENDA);
+
+    if (!doctor) {
+      throw new Error("No doctor found");
+    }
+
+    return doctor;
+  }
+}
