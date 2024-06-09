208216bfffc7b36a6f05e298f957fcdd7afb8181
carvalhaus <ooliveira.joaop@gmail.com>
2024-06-09 14:41:50 -0300

    feat: create sign-in and sign-up pages

--- app/app/sign-in/page.tsx
+++ app/app/sign-in/page.tsx
@@ -1,5 +1,58 @@
+import { Button } from "@/components/button";
+import Field from "@/components/form";
+import Header from "@/components/header";
+import Link from "next/link";
+
 function SignIn() {
-  return <h1>SIGN IN</h1>;
+  return (
+    <>
+      <Header href="/" title="Login" iconClassName="w-4 h-4" />
+      <div className="py-4">
+        <h1 className="font-bold text-xl">Acesse sua conta</h1>
+        <p>Olá, informe seus dados para acessar.</p>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <Field
+          labelProps={{
+            children: "Telefone",
+          }}
+          inputProps={{
+            placeholder: "Informe seu numero de telefone",
+            type: "text",
+            defaultValue: "",
+            autoFocus: true,
+          }}
+        />
+        <Field
+          labelProps={{
+            children: "Senha",
+          }}
+          inputProps={{
+            placeholder: "Insira sua senha",
+            type: "password",
+            defaultValue: "",
+            autoFocus: true,
+            className: "mb-1",
+          }}
+        />
+        <Button className="mb-6">Login</Button>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex h-6 relative w-full items-center justify-center mb-6">
+          <span className="absolute z-10 bg-white px-2 text-sm">ou</span>
+          <div className="border-b border-b-gray-100 w-full absolute"></div>
+        </div>
+        <p>
+          Não está cadastrado?{" "}
+          <Link href="/sign-up" className="text-[#459487] font-bold underline">
+            Crie sua conta
+          </Link>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default SignIn;
--- app/app/sign-up/page.tsx
+++ app/app/sign-up/page.tsx
@@ -1,5 +1,65 @@
+import { Button } from "@/components/button";
+import Field from "@/components/form";
+import Header from "@/components/header";
+import Link from "next/link";
+
 function SignUp() {
-  return <h1>SIGN UP</h1>;
+  return (
+    <>
+      <Header href="/" title="Criar conta" iconClassName="w-4 h-4" />
+      <div className="py-4">
+        <h1 className="font-bold text-xl">Criar uma conta</h1>
+        <p>Por favor, informe seu nome e uma senha para criar sua conta.</p>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <Field
+          labelProps={{
+            children: "Seu nome",
+          }}
+          inputProps={{
+            placeholder: "Informe seu nome",
+            type: "text",
+            defaultValue: "",
+            autoFocus: true,
+          }}
+        />
+        <Field
+          labelProps={{
+            children: "Telefone",
+          }}
+          inputProps={{
+            placeholder: "Informe seu numero de telefone",
+            type: "text",
+            defaultValue: "",
+            autoFocus: true,
+          }}
+        />
+        <Field
+          labelProps={{
+            children: "Senha",
+          }}
+          inputProps={{
+            placeholder: "Insira sua senha",
+            type: "password",
+            defaultValue: "",
+            autoFocus: true,
+            className: "mb-1",
+          }}
+        />
+        <Button className="mb-6">Criar conta</Button>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
+        <p>
+          Já está cadastrado?{" "}
+          <Link href="/sign-in" className="text-[#459487] font-bold underline">
+            Acesse sua conta
+          </Link>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
